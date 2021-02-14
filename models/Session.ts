a494b3949a4e1c1b4a22c191f1fb7c6905ebcefd
keidai0702 <keidai0702@icloud.com>
2021-02-14 17:04:31 +0900

    add sigin table

---
+++ models/Session.ts
@@ -0,0 +1,29 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn, OneToOne } from 'typeorm'
+import { UserEntity } from './User';
+
+@Entity({ name: 'sessions' })
+export class SessionEntity {
+  @PrimaryColumn({ type: 'uuid' })
+  public id!: string
+
+  @Column({ type: 'uuid' })
+  @OneToOne(() => UserEntity)
+  public userId!: string;
+
+  @Column({ type: 'varchar' })
+  public token!: string
+
+  @CreateDateColumn({ name:'created_at', type: 'timestamp' })
+  public createdAt!: Date
+
+	constructor(props: Partial<SessionEntity>) {
+    if (props) {
+      if (props.userId) {
+        this.userId = props.userId;
+      }
+      if (props.token) {
+        this.token = props.token;
+      }
+    }
+  }
+}
